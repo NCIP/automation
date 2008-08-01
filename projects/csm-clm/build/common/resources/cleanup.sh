c74c8cc5cb5ca1659c8ffd64f602d8503b46633c
gursesl <gursesl@ncias-d1611-v.nci.nih.gov>
2008-08-01 15:45:53 +0000

    Removed file/folder
    Former-commit-id: a2ab70b449b038f5168c27ebe67667de91433cf9

--- projects/csm-clm/build/common/resources/cleanup.sh
+++
@@ -1,8 +0,0 @@
-rm_file_list="ApplicationSecurityConfig.xml AuthSchemaMySQL.sql AuthSchemaOracle.sql AuthSchemaPostgres.sql AuthSchemaSQLServer.sql DataPrimingMySQL.sql DataPrimingOracle.sql DataPrimingPostgres.sql DataPrimingSQLServer.sql hibernate.cfg.xml log4jConfig.xml login-config.db-block.xml login-config.ldap-block.xml MigrationScript3.2MySQL.sql MigrationScript3.2Oracle.sql MigrationScript4.0MySQL.sql MigrationScript4.0Oracle.sql mysql-ds.xml ObjectStateLoggerConfig.xml oracle-ds.xml PasswordEncrypter.java postgres-ds.xml sqlserver-ds.xml"
-
-for file in $rm_file_list
-do
-	svn remove  ../../api/$file
-	svn remove  ../../securityws/$file
-	svn remove  ../../upt/$file
-done
