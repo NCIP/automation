cd9120c06e25fe38a8f13a63569b84f5f967c05c
gursesl <gursesl@ncias-d1611-v.nci.nih.gov>
2008-08-05 03:27:07 +0000

    Former-commit-id: e5222869357be632b9ec05052b46735876b9bc2b

--- software/bda-build-template/software/common/resources_blueprints_webapp/db-install/create-database-mysql.sql
+++ software/bda-build-template/software/common/resources_blueprints_webapp/db-install/create-database-mysql.sql
@@ -1,8 +1,8 @@
 DROP DATABASE IF EXISTS bda-blueprints;
 CREATE DATABASE bda-blueprints;
 
-GRANT ALL PRIVILEGES ON bda-blueprints.* TO 'bdauser'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON bda-blueprints.* TO 'bdauser'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON bda-blueprints.* TO 'bdauser'@'localhost' IDENTIFIED BY 'bdapwd' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON bda-blueprints.* TO 'bdauser'@'%' IDENTIFIED BY 'bdapwd' WITH GRANT OPTION;
 
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost'
     IDENTIFIED BY 'sa' WITH GRANT OPTION;
