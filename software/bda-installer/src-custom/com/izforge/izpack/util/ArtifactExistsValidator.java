a6c9b100869433f5febedca0f8a125f307e16124
narram <narram@ncias-d1611-v.nci.nih.gov>
2008-12-11 18:34:22 +0000

    #18250 loggers
    Former-commit-id: 3c6559656c1a8a804d7f57ba31e0bd42443d012d

--- software/bda-installer/src-custom/com/izforge/izpack/util/ArtifactExistsValidator.java
+++ software/bda-installer/src-custom/com/izforge/izpack/util/ArtifactExistsValidator.java
@@ -1,8 +1,5 @@
 package com.izforge.izpack.util;
 
-import com.izforge.izpack.panels.ProcessingClient;
-import com.izforge.izpack.panels.Validator;
-
 import java.util.Map;
 import java.io.File;
 
@@ -13,27 +10,39 @@ import java.io.File;
  *
  * @author Douglas Harley <harleyda@mail.nih.gov>
  */
-public class ArtifactExistsValidator implements Validator
+public class ArtifactExistsValidator implements com.izforge.izpack.panels.Validator
 {
+
+	static {
+		System.out.println("ArtifactExistsValidator class is loaded!");
+	}
 
     private static final String ARTIFACT_PATH_PARAM = "artifactPath";
     private static final String IS_DIR_PARAM = "isDir";
 
-    public boolean validate(ProcessingClient client) {
-        if(!client.hasParams()) {
-			throw new RuntimeException("Required params missing: 'artifactPath', 'isDir'.");
-        }
-		Map<String, String> paramMap = client.getValidatorParams();
-		String artifactPath = paramMap.get(ARTIFACT_PATH_PARAM);
-		System.out.println("artifactPath =" + artifactPath + "=");
-		boolean isDir = Boolean.valueOf(paramMap.get(IS_DIR_PARAM)).booleanValue();
-		System.out.println("isDir =" + isDir + "=");
-		File artifactFile = new File(artifactPath);
-		boolean artifactExists = artifactFile.exists();
-		if(artifactExists && isDir) {
-			artifactExists = artifactFile.isDirectory();
+    public boolean validate(com.izforge.izpack.panels.ProcessingClient client) {
+		System.out.println("starting validate(com.izforge.izpack.panels.ProcessingClient)...");
+		boolean artifactExists = false;
+		try {
+			if(!client.hasParams()) {
+				throw new RuntimeException("Required params missing: 'artifactPath', 'isDir'.");
+			}
+			Map<String, String> paramMap = client.getValidatorParams();
+			String artifactPath = paramMap.get(ARTIFACT_PATH_PARAM);
+			System.out.println("artifactPath =" + artifactPath + "=");
+			boolean isDir = Boolean.valueOf(paramMap.get(IS_DIR_PARAM)).booleanValue();
+			System.out.println("isDir =" + isDir + "=");
+			File artifactFile = new File(artifactPath);
+			artifactExists = artifactFile.exists();
+			if(artifactExists && isDir) {
+				artifactExists = artifactFile.isDirectory();
+			}
 		}
-		System.out.println("exiting: artifactExists =" + artifactExists + "=");
+		catch(Exception exception) {
+			System.err.println("Cannot validate if artifact exists: " + exception.getMessage());
+			exception.printStackTrace(System.err);
+		}
+		System.out.println("exiting validate(com.izforge.izpack.panels.ProcessingClient): artifactExists =" + artifactExists + "=");
         return artifactExists;
     }
 
