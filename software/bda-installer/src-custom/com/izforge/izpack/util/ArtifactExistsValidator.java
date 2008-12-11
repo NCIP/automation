69a4e9bace581d2db343ce6756b64b3bcdac1f0b
narram <narram@ncias-d1611-v.nci.nih.gov>
2008-12-11 16:19:20 +0000

    #18250 validation to check if the artifact path exists
    Former-commit-id: a0f10a7241df6c573465542734131c556cbb542d

---
+++ software/bda-installer/src-custom/com/izforge/izpack/util/ArtifactExistsValidator.java
@@ -0,0 +1,37 @@
+package com.izforge.izpack.util;
+
+import com.izforge.izpack.panels.ProcessingClient;
+import com.izforge.izpack.panels.Validator;
+
+import java.util.Map;
+import java.io.File;
+
+/**
+ * A validator to enforce that a specified artifact exists on the filesystem.
+ * <p/>
+ * This validator will check that the specified artifact exists on the filesystem.
+ *
+ * @author Douglas Harley <harleyda@mail.nih.gov>
+ */
+public class ArtifactExistsValidator implements Validator
+{
+
+    private static final String ARTIFACT_PATH_PARAM = "artifactPath";
+    private static final String IS_DIR_PARAM = "isDir";
+
+    public boolean validate(ProcessingClient client) {
+        if(!client.hasParams()) {
+			throw new RuntimeException("Required params missing: 'artifactPath', 'isDir'.");
+        }
+		Map<String, String> paramMap = client.getValidatorParams();
+		String artifactPath = paramMap.get(ARTIFACT_PATH_PARAM);
+		boolean isDir = Boolean.valueOf(paramMap.get(IS_DIR_PARAM)).booleanValue();
+		File artifactFile = new File(artifactPath);
+		boolean artifactExists = artifactFile.exists();
+		if(artifactExists && isDir) {
+			artifactExists = artifactFile.isDirectory();
+		}
+        return artifactExists;
+    }
+
+}
