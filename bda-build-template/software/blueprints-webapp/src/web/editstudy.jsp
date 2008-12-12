87fea8e5ffaa15af89c514e6a7f7922cadd15804
saksass <saksass@ncias-d1611-v.nci.nih.gov>
2008-12-12 20:52:56 +0000

    0.7.0 release

---
+++ bda-build-template/software/blueprints-webapp/src/web/editstudy.jsp
@@ -0,0 +1,37 @@
+<html>
+<title>bda-blueprints-webapp</title>
+<head>
+<link href="stylesheets/onsq.css" rel="stylesheet" type="text/css" />
+</head>
+
+<BODY BGCOLOR="White" LINK="#FFAA00" VLINK="#FFAA00" ALINK="#330066"
+	LEFTMARGIN=0 TOPMARGIN=0 MARGINWIDTH="0" MARGINHEIGHT="0">
+<FORM action="/bda-blueprints-webapp/Controller" method="post"><INPUT
+	TYPE="hidden" NAME="FROM_PAGE" VALUE="editstudy"> <INPUT
+	TYPE="hidden" NAME="TO_PAGE" VALUE="study">
+<table align="center">
+	<tr>
+		<td colspan="2"><b>Study</b></td>
+	</tr>
+	<tr>
+		<td>Study Name:</td>
+		<td><input type="text" name="study_name" id="study_name" value=""
+			length="30" /></td>
+	</tr>
+	<tr>
+		<td>Researcher:</td>
+		<td><input type="text" name="researcher" id="researcher" value=""
+			length="30" /></td>
+	</tr>
+	<tr>
+		<td>Date Received:</td>
+		<td><input type="text" name="date_received" id="date_received"
+			value="" length="30" /></td>
+	</tr>
+	<tr>
+		<td><input type="submit" name="save" id="save" value="Save" /></td>
+	</tr>
+</table>
+</FORM>
+</body>
+</html>
---
+++ bda-installer/src-custom/com/izforge/izpack/util/ArtifactExistsValidator.java
@@ -0,0 +1,49 @@
+package com.izforge.izpack.util;
+
+import java.util.Map;
+import java.io.File;
+
+/**
+ * A validator to enforce that a specified artifact exists on the filesystem.
+ * <p/>
+ * This validator will check that the specified artifact exists on the filesystem.
+ *
+ * @author Douglas Harley <harleyda@mail.nih.gov>
+ */
+public class ArtifactExistsValidator implements com.izforge.izpack.panels.Validator
+{
+
+	static {
+		System.out.println("ArtifactExistsValidator class is loaded!");
+	}
+
+    private static final String ARTIFACT_PATH_PARAM = "artifactPath";
+    private static final String IS_DIR_PARAM = "isDir";
+
+    public boolean validate(com.izforge.izpack.panels.ProcessingClient client) {
+		System.out.println("starting validate(com.izforge.izpack.panels.ProcessingClient)...");
+		boolean artifactExists = false;
+		try {
+			if(!client.hasParams()) {
+				throw new RuntimeException("Required params missing: 'artifactPath', 'isDir'.");
+			}
+			Map<String, String> paramMap = client.getValidatorParams();
+			String artifactPath = paramMap.get(ARTIFACT_PATH_PARAM);
+			System.out.println("artifactPath =" + artifactPath + "=");
+			boolean isDir = Boolean.valueOf(paramMap.get(IS_DIR_PARAM)).booleanValue();
+			System.out.println("isDir =" + isDir + "=");
+			File artifactFile = new File(artifactPath);
+			artifactExists = artifactFile.exists();
+			if(artifactExists && isDir) {
+				artifactExists = artifactFile.isDirectory();
+			}
+		}
+		catch(Exception exception) {
+			System.err.println("Cannot validate if artifact exists: " + exception.getMessage());
+			exception.printStackTrace(System.err);
+		}
+		System.out.println("exiting validate(com.izforge.izpack.panels.ProcessingClient): artifactExists =" + artifactExists + "=");
+        return artifactExists;
+    }
+
+}
