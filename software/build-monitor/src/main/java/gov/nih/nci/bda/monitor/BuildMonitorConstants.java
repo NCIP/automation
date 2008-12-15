c6033f1dd8f768f011ce8aaea1f8d0f778cdd3d1
narram <narram@ncias-d1611-v.nci.nih.gov>
2008-12-15 15:50:01 +0000

    initial checkin for the build monitor
    Former-commit-id: b7420708547613573a01a8c22dcfc7a947b26391

---
+++ software/build-monitor/src/main/java/gov/nih/nci/bda/monitor/BuildMonitorConstants.java
@@ -0,0 +1,8 @@
+package gov.nih.nci.bda.monitor;
+
+public class BuildMonitorConstants {
+	public static final String BUILD_FAILED = "FAILED";
+	public static final String BUILD_SUCCESSFUL = "SUCCESSFUL";
+	public static final String WIKI_SUCCESSFUL = "(/)";
+	public static final String WIKI_FAILED = "(x)";	
+}
---
+++ software/build-monitor/src/main/java/gov/nih/nci/bda/monitor/BuildMonitorHelper.java
@@ -0,0 +1,233 @@
+package gov.nih.nci.bda.monitor;
+
+import gov.nih.nci.bda.monitor.business.BuildMonitorBean;
+import gov.nih.nci.bda.monitor.domain.BuildHistory;
+import gov.nih.nci.bda.monitor.domain.BuildStatus;
+import gov.nih.nci.bda.monitor.domain.ProjectBuildStatus;
+import gov.nih.nci.bda.monitor.util.HibernateUtil;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.Timestamp;
+import java.util.Iterator;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+public class BuildMonitorHelper {
+
+	BuildMonitorBean bmb;
+	
+	public BuildMonitorHelper()
+	{
+		super();
+	}
+
+	public BuildMonitorHelper(BuildMonitorBean bmb)
+	{
+		this.bmb = bmb;
+	}
+	
+	public void updateBuildStatus()
+	{		
+		BuildStatus bs = null;
+		String buildTier = getTierName(bmb.getPropertiesFileName());
+		if (!bmb.isBuildSuccessful()) 
+		{									
+		    Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		    session.beginTransaction(); 		    
+		    
+		    Query query = session.createQuery( "from BuildStatus as status where buildTier= :buildTier");
+		    query.setString("buildTier", buildTier);
+			Iterator it=query.iterate();			
+			if(it != null && it.hasNext())
+		    {		    	
+		    	bs = (BuildStatus) it.next();		    	
+		    	bs.setBuildStatus(BuildMonitorConstants.BUILD_FAILED);
+		    	bs.setWikiBuildStatus(BuildMonitorConstants.WIKI_FAILED);
+		    	bs.setLastBuildTime(new Timestamp(System.currentTimeMillis()));
+		    	session.update(bs);
+		    }
+		    else
+		    {
+		    	bs = new BuildStatus();
+		    	bs.setBuildTier(buildTier);
+		    	bs.setBuildStatus(BuildMonitorConstants.BUILD_FAILED);
+		    	bs.setWikiBuildStatus(BuildMonitorConstants.WIKI_FAILED);
+		    	bs.setLastBuildTime(new Timestamp(System.currentTimeMillis()));
+		    	session.save(bs);		    	
+		    }
+
+	    	BuildHistory bh = new BuildHistory();			  
+	    	bh.setBuildTier(buildTier);
+	    	bh.setBuildStatus(BuildMonitorConstants.BUILD_FAILED);
+	    	bh.setWikiBuildStatus(BuildMonitorConstants.WIKI_FAILED);
+	    	bh.setLastBuildTime(new Timestamp(System.currentTimeMillis()));
+	    	session.save(bh);	
+		    	
+		    session.getTransaction().commit();		    
+		}
+		else
+		{						
+		    Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		    session.beginTransaction(); 		    
+		    Query query = session.createQuery( "from BuildStatus where buildTier= '"+buildTier+"'");
+			bs= (BuildStatus) query.uniqueResult();
+
+			if(bs != null )
+			{				
+		    	bs.setBuildStatus(BuildMonitorConstants.BUILD_SUCCESSFUL);
+		    	bs.setWikiBuildStatus(BuildMonitorConstants.WIKI_SUCCESSFUL);
+		    	bs.setLastBuildTime(new Timestamp(System.currentTimeMillis()));
+		    	session.update(bs);
+		    }
+		    else
+		    {
+		    	bs = new BuildStatus();
+		    	bs.setBuildTier(buildTier);
+		    	bs.setBuildStatus(BuildMonitorConstants.BUILD_SUCCESSFUL);
+		    	bs.setWikiBuildStatus(BuildMonitorConstants.WIKI_SUCCESSFUL);
+		    	bs.setLastBuildTime(new Timestamp(System.currentTimeMillis()));
+		    	session.save(bs);		    	
+		    }
+
+	    	BuildHistory bh = new BuildHistory();			  
+	    	bh.setBuildTier(buildTier);
+	    	bh.setBuildStatus(BuildMonitorConstants.BUILD_SUCCESSFUL);
+	    	bh.setWikiBuildStatus(BuildMonitorConstants.WIKI_SUCCESSFUL);
+	    	bh.setLastBuildTime(new Timestamp(System.currentTimeMillis()));
+	    	session.save(bh);
+		    	
+		    session.getTransaction().commit();
+		}
+
+	}
+
+	
+	
+	public void updateProjectBuildStatus()
+	{				
+		ProjectBuildStatus pbs = null;
+		String buildTier = getTierName(bmb.getPropertiesFileName());
+		String projectName = bmb.getProjectName();
+		String methodName = "set"+buildTier;
+	    Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+	    session.beginTransaction(); 		    
+		
+		if (!bmb.isBuildSuccessful()) 
+		{											    		    
+		    Query query = session.createQuery( " from ProjectBuildStatus where product= :projectName ");
+		    query.setString("projectName", projectName);			
+		    Iterator it=query.iterate();			
+			if(it != null && it.hasNext())
+		    {		    	
+		    	pbs = (ProjectBuildStatus) it.next();	
+		    	try {
+					pbs.getClass().getMethod(methodName, new Class[] {String.class}).invoke(pbs,BuildMonitorConstants.WIKI_FAILED); 
+				}
+		    	catch (Exception e) {				
+					e.printStackTrace();
+				}
+		    	session.update(pbs);
+		    }
+		    else
+		    {
+		    	pbs = new ProjectBuildStatus();
+		    	pbs.setProduct(projectName);
+		    	pbs.setDev(BuildMonitorConstants.WIKI_FAILED);
+		    	pbs.setQa(BuildMonitorConstants.WIKI_FAILED);
+		    	pbs.setStage(BuildMonitorConstants.WIKI_FAILED);
+		    	pbs.setProd(BuildMonitorConstants.WIKI_FAILED);
+		    	session.save(pbs);		    	
+		    }
+
+	    	BuildHistory bh = new BuildHistory();
+	    	bh.setProduct(projectName);
+	    	bh.setBuildTier(buildTier);
+	    	bh.setBuildStatus(BuildMonitorConstants.BUILD_FAILED);
+	    	bh.setWikiBuildStatus(BuildMonitorConstants.WIKI_FAILED);
+	    	bh.setLastBuildTime(new Timestamp(System.currentTimeMillis()));
+	    	session.save(bh);	
+		    	
+		    
+		}
+		else
+		{						
+		    
+		    Query query = session.createQuery( " from ProjectBuildStatus where product= :projectName ");
+		    query.setString("projectName", projectName);
+			pbs= (ProjectBuildStatus) query.uniqueResult();
+
+			if(pbs != null )
+			{						    		
+		    	try {
+					pbs.getClass().getMethod(methodName, new Class[] {String.class}).invoke(pbs,BuildMonitorConstants.WIKI_SUCCESSFUL); 
+				}
+		    	catch (Exception e) {				
+					e.printStackTrace();
+				}
+		    	session.update(pbs);
+		    }
+		    else
+		    {
+		    	pbs = new ProjectBuildStatus();
+		    	Method[] methods = pbs.getClass().getMethods();
+		    	for (int i = 0; i < methods.length; i++)
+		    	{
+		    		if(methods[i].getName().equals(methodName))
+    				{
+		    			try 
+		    			{
+		    				methods[i].invoke(pbs,new String(BuildMonitorConstants.WIKI_SUCCESSFUL));
+		    			}
+				    	catch (Exception e) {				
+							e.printStackTrace();
+						}
+    				}
+		    		else if(methods[i].getName().startsWith("set"))
+		    		{
+		    			if(!(methods[i].getName().equals("setProduct") || methods[i].getName().equals("setId")))
+						{
+			    			try 
+			    			{
+			    				methods[i].invoke(pbs,new String(BuildMonitorConstants.WIKI_FAILED));
+							}
+			    			catch (Exception e) 
+			    			{				
+			    				e.printStackTrace();
+			    			}
+						}
+		    		}
+		    	}
+		    	pbs.setProduct(projectName);
+		    	session.save(pbs);		    	
+		    }
+
+	    	BuildHistory bh = new BuildHistory();		  
+	    	bh.setProduct(projectName);
+	    	bh.setBuildTier(buildTier);
+	    	bh.setBuildStatus(BuildMonitorConstants.BUILD_SUCCESSFUL);
+	    	bh.setWikiBuildStatus(BuildMonitorConstants.WIKI_SUCCESSFUL);
+	    	bh.setLastBuildTime(new Timestamp(System.currentTimeMillis()));
+	    	session.save(bh);
+		    
+		}
+	    session.getTransaction().commit();
+	}
+	
+	
+	private String getTierName(String  propertyFile) {
+		String tierName = "local";
+				
+		if(propertyFile.contains("dev-"))
+			tierName="Dev";
+		if(propertyFile.contains("qa-"))
+			tierName="Qa";
+		if(propertyFile.contains("stage-"))
+			tierName="Stage";
+		if(propertyFile.contains("prod-"))
+			tierName="Prod";
+		
+		return tierName;
+	}
+}
