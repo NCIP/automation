c59698fc8dea12e3daa427a1e28ba475e4076d89
narram <narram@ncias-d1611-v.nci.nih.gov>
2009-01-13 15:49:12 +0000

    schema and reference data for certification dashboard
    Former-commit-id: 301f25e61c1565738db8014aab9b036e4a0449a7

---
+++ software/bda-dashboard/certification-monitor/src/main/sql/insert_target_lookup.sql
@@ -0,0 +1,3 @@
+insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('validate:JAD','singleCommandBuild','1');
+insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('validate:svn:checkout-project','singleCommandBuild','2');
+insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('build:project','singleCommandBuild','3');
