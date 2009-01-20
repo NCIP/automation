97fb8eba3e69d4e5a9c7a2d2f117385ea787d849
narram <narram@ncias-d1611-v.nci.nih.gov>
2009-01-20 21:39:02 +0000

    testing for database integration
    Former-commit-id: 0f1891c0704826b47a95ce7acf66df1341ada89a

--- software/bda-dashboard/certification-monitor/src/main/sql/insert_target_lookup.sql
+++ software/bda-dashboard/certification-monitor/src/main/sql/insert_target_lookup.sql
@@ -1,3 +1,17 @@
+/*
+SQLyog Community Edition- MySQL GUI
+MySQL - 5.0.27-community-nt 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `target_lookup` (
+	`TARGET_NAME` varchar (300),
+	`MAP_NAME` varchar (300),
+	`id` double 
+); 
 insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('validate:JAD','singleCommandBuild','1');
 insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('validate:svn:checkout-project','singleCommandBuild','2');
-insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('build:project','singleCommandBuild','3');
+insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('build:single-command-build','singleCommandBuild','3');
+insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('build:single-command-deployment','singleCommandDeployment','4');
+insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('build:database-integration','databaseIntegration','5');
