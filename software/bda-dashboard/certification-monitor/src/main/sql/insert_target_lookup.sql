58331ad7a9e8cfcf605bb02996eaf65456c53097
narram <narram@ncias-d1611-v.nci.nih.gov>
2009-01-28 20:37:50 +0000

    updated sql files
    Former-commit-id: fcd6283c9c1e584a6128420b7564ecf5ea9909c0

--- software/bda-dashboard/certification-monitor/src/main/sql/insert_target_lookup.sql
+++ software/bda-dashboard/certification-monitor/src/main/sql/insert_target_lookup.sql
@@ -1,17 +1,21 @@
 /*
 SQLyog Community Edition- MySQL GUI
-MySQL - 5.0.27-community-nt 
+MySQL - 5.0.45 
 *********************************************************************
 */
 /*!40101 SET NAMES utf8 */;
 
 create table `target_lookup` (
-	`TARGET_NAME` varchar (300),
-	`MAP_NAME` varchar (300),
-	`id` double 
+	`ID` double ,
+	`TARGET_NAME` varchar (150),
+	`MAP_NAME` varchar (150),
+	`IS_VALUE` varchar (15)
 ); 
-insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('validate:JAD','singleCommandBuild','1');
-insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('validate:svn:checkout-project','singleCommandBuild','2');
-insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('build:single-command-build','singleCommandBuild','3');
-insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('build:single-command-deployment','singleCommandDeployment','4');
-insert into `target_lookup` (`TARGET_NAME`, `MAP_NAME`, `id`) values('build:database-integration','databaseIntegration','5');
+insert into `target_lookup` (`ID`, `TARGET_NAME`, `MAP_NAME`, `IS_VALUE`) values('1','validate:JAD','singleCommandBuild',NULL);
+insert into `target_lookup` (`ID`, `TARGET_NAME`, `MAP_NAME`, `IS_VALUE`) values('2','validate:svn:checkout-project','singleCommandBuild',NULL);
+insert into `target_lookup` (`ID`, `TARGET_NAME`, `MAP_NAME`, `IS_VALUE`) values('3','build:project','singleCommandBuild',NULL);
+insert into `target_lookup` (`ID`, `TARGET_NAME`, `MAP_NAME`, `IS_VALUE`) values('4','build:single-command-deployment','singleCommandDeployment',NULL);
+insert into `target_lookup` (`ID`, `TARGET_NAME`, `MAP_NAME`, `IS_VALUE`) values('5','build:database-integration','databaseIntegration',NULL);
+insert into `target_lookup` (`ID`, `TARGET_NAME`, `MAP_NAME`, `IS_VALUE`) values('6','build:bda-utils','bdaUtilsVersion','true');
+insert into `target_lookup` (`ID`, `TARGET_NAME`, `MAP_NAME`, `IS_VALUE`) values('7','build:template-properties','templateValidation',NULL);
+insert into `target_lookup` (`ID`, `TARGET_NAME`, `MAP_NAME`, `IS_VALUE`) values('8','build:private-repository-properties','privateRepositoryProperties',NULL);
