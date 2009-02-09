329eb35e3c63bdf13be7b5c4002bda45864ce6f1
narram <narram@ncias-d1611-v.nci.nih.gov>
2009-02-09 21:06:32 +0000

    update the differed constant
    Former-commit-id: 048c7626cc53334b7fffca3eddc545ba6b214c52

--- software/bda-dashboard/certification-monitor/src/main/java/gov/nih/nci/bda/certification/BuildCertificationConstants.java
+++ software/bda-dashboard/certification-monitor/src/main/java/gov/nih/nci/bda/certification/BuildCertificationConstants.java
@@ -6,6 +6,6 @@ public class BuildCertificationConstants
 	public static final String BUILD_SUCCESSFUL = "SUCCESSFUL";
 	public static final String WIKI_SUCCESSFUL = "(/)";
 	public static final String WIKI_FAILED = "(x)";
-	public static final String WIKI_NOTBUILD = "(*y)";
+	public static final String WIKI_NOTBUILD = "(off)";
 	public static final String ANTHILL_ADDRESS = "http://cbiocvs2.nci.nih.gov:8080/AnthillPro/intranet/";
 }
