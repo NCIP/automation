216bbe8c72ae3ec5f1fc58867b2b0efbc09786d4
narram <narram@ncias-d1611-v.nci.nih.gov>
2009-03-09 22:29:58 +0000

    test the load properties
    Former-commit-id: eba31aa426602f07a20c79b746978fba84c4d701

--- software/bda-dashboard/certification-monitor/src/main/sql/insert_target_lookup.sql
+++ software/bda-dashboard/certification-monitor/src/main/sql/insert_target_lookup.sql
@@ -7,14 +7,14 @@ create table target_lookup (
 	MAP_NAME varchar (150),
 	IS_VALUE varchar (15)
 ); 
-insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values('1','validate:JAD','singleCommandBuild',NULL);
-insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values('2','validate:svn:checkout-project','singleCommandBuild',NULL);
-insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values('3','build:project','singleCommandBuild',NULL);
-insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values('4','build:single-command-deployment','singleCommandDeployment',NULL);
-insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values('5','build:database-integration','databaseIntegration',NULL);
-insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values('9','build:ci-server','latestCIBuild','true');
-insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values('6','build:check-bdafied','bdaEnabled','true');
-insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values('7','build:template-properties','templateValidation',NULL);
-insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values('8','build:private-repository-properties','privateRepositoryProperties',NULL);
-insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values('10','build:validate-app-servers','deploymentShakeout',NULL);
-insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values('11','build:check-commandline-installer','commandLineInstaller',NULL);
+insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(1,'validate:JAD','singleCommandBuild',NULL);
+insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(2,'validate:svn:checkout-project','singleCommandBuild',NULL);
+insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(3,'build:project','singleCommandBuild',NULL);
+insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(4,'build:single-command-deployment','singleCommandDeployment',NULL);
+insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(5,'build:database-integration','databaseIntegration',NULL);
+insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(9,'build:ci-server','latestCIBuild','true');
+insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(6,'build:check-bdafied','bdaEnabled','true');
+insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(7,'build:template-properties','templateValidation',NULL);
+insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(8,'build:private-repository-properties','privateRepositoryProperties',NULL);
+insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(10,'build:validate-app-servers','deploymentShakeout',NULL);
+insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(11,'build:check-commandline-installer','commandLineInstaller',NULL);
