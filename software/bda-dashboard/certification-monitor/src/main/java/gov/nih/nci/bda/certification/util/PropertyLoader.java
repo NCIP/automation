76a45f4f95fdf024efe70bb990d9a4f1cdb3583f
narram <narram@ncias-d1611-v.nci.nih.gov>
2009-03-09 20:51:10 +0000

    test the load properties
    Former-commit-id: 0426084a186d673967dd7c06c548734b30b97e8b

--- software/bda-dashboard/certification-monitor/src/main/java/gov/nih/nci/bda/certification/util/PropertyLoader.java
+++ software/bda-dashboard/certification-monitor/src/main/java/gov/nih/nci/bda/certification/util/PropertyLoader.java
@@ -15,9 +15,10 @@ import org.apache.tools.ant.Project;
 public class PropertyLoader {
 
 	private static ProjectProperties pp = null;
+	private static Configuration config = ConfigurationHelper.getConfiguration();
 	
 	public static void loadProjectProperties(String projectName,Project project) {
-		Configuration config = ConfigurationHelper.getConfiguration();
+		
         Iterator it = config.getKeys(projectName);
        
         project.setProperty("project.name", projectName);
@@ -30,20 +31,32 @@ public class PropertyLoader {
         	project.setProperty(keyName, config.getString(keyName));
         }
         
+        loadDatabaseProperties(config.getString(projectName + ".database.type"),project);
 	}
 
-	public static void loadGeneralProperties(Project project) {
-		Configuration config = ConfigurationHelper.getConfiguration();
-       
+	private static void loadDatabaseProperties(String databaseType,Project project) {
+        project.setProperty(databaseType+".database.system.user",  config.getString(databaseType+".database.system.user"));
+        project.setProperty(databaseType+".database.system.password",  config.getString(databaseType+".database.system.password"));
+        project.setProperty(databaseType+".database.server",  config.getString(databaseType+".database.server"));
+        project.setProperty(databaseType+".database.port",  config.getString(databaseType+".database.port"));
+        project.setProperty(databaseType+".database.name",  config.getString(databaseType+".database.name"));
+        
+        project.setProperty(databaseType+".database.user",  config.getString(databaseType+".database.user"));
+        project.setProperty(databaseType+".database.password",  config.getString(databaseType+".database.password"));
+        project.setProperty(databaseType+".database.url",  config.getString(databaseType+".database.url"));
+        project.setProperty(databaseType+".database.system.url",  config.getString(databaseType+".database.system.url"));
+        project.setProperty(databaseType+".minimum.version",  config.getString(databaseType+".minimum.version"));
+	}
+
+	public static void loadGeneralProperties(Project project) {       
         project.setProperty("ant.minimum.version",  config.getString("ant.minimum.version"));
         project.setProperty("java.major.version",  config.getString("java.major.version"));
         project.setProperty("java.minor.version",  config.getString("java.minor.version"));
         project.setProperty("bda.version",  config.getString("bda.version"));
-        project.setProperty("force.reinstall",  config.getString("force.reinstall"));
-        
+        project.setProperty("force.reinstall",  config.getString("force.reinstall"));        
 	}
+	
 	public static void loadProperties(String projectName) {
-		Configuration config = ConfigurationHelper.getConfiguration();
         Iterator it = config.getKeys(projectName);
         while(it.hasNext())
         {
