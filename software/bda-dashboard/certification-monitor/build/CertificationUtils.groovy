95566b54d7f7c94bdb9df2176a278627ef9e50f5
narram <narram@ncias-d1611-v.nci.nih.gov>
2009-03-18 21:07:40 +0000

    add the bda utils version in the error message
    Former-commit-id: 6d1143fd3eee4299b114c38e6ce4e73604729b00

--- software/bda-dashboard/certification-monitor/build/CertificationUtils.groovy
+++ software/bda-dashboard/certification-monitor/build/CertificationUtils.groovy
@@ -64,7 +64,7 @@ class CertificationUtils
 		        if(bdaVersion.endsWith("beta"))
 		        {
 		        	project.setProperty("is.value","false");
-		        	ant.fail("PROJECT USES THE BETA VERSION OF THE BDA UTILS")
+		        	ant.fail("PROJECT USES THE " + bdaVersion+ " BETA VERSION OF THE BDA UTILS")
 		        }else
 		        {
 				StringBuffer wikiStr = new StringBuffer("'[");
