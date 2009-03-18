3f3c34914ef8eae47bc239d48db5bdc3f6bf9c6c
narram <narram@ncias-d1611-v.nci.nih.gov>
2009-03-18 17:58:22 +0000

    change the name of the single command build target
    Former-commit-id: bc54fa67f0836a12d0e7148b933486819b8fd7bd

--- software/bda-dashboard/certification-monitor/src/main/sql/insert_target_lookup.sql
+++ software/bda-dashboard/certification-monitor/src/main/sql/insert_target_lookup.sql
@@ -1,6 +1,6 @@
 insert into target_lookup (ID, TARGET_NAME, MAP_NAME) values(1,'validate:JAD','singleCommandBuild');
 insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(2,'validate:svn:checkout-project','singleCommandBuild',NULL);
-insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(3,'build:project','singleCommandBuild',NULL);
+insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(3,'build:single-command-build','singleCommandBuild',NULL);
 insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(4,'build:single-command-deployment','singleCommandDeployment',NULL);
 insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(5,'build:database-integration','databaseIntegration',NULL);
 insert into target_lookup (ID, TARGET_NAME, MAP_NAME, IS_VALUE) values(9,'build:ci-server','latestCIBuild','true');
