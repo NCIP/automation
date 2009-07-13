3fa3e724bb590c9df70250a5f1cf1fff05434ba3
narram <narram@ncias-d1611-v.nci.nih.gov>
2009-07-13 16:33:07 +0000

    add the license
    Former-commit-id: d9acf81211a7f300563723fce0345e8825b70687

--- software/provisioner/src/main/java/gov/nih/nci/bda/provisioner/EC2SystemInitiator.java
+++ software/provisioner/src/main/java/gov/nih/nci/bda/provisioner/EC2SystemInitiator.java
@@ -1,4 +1,32 @@
+/*
+* The caBIG software, the software subject to this notice and license, includes both human readable source code form and machine readable, binary, object code form (Software). The Software was developed for and in conjunction with the National Cancer Institute (NCI) by Stelligent (an Automation for the People, LLC company). To the extent government employees are authors, any rights in such works shall be subject to Title 17 of the United States Code,
+* section 105.
+
+* This Software License (License) is between NCI and You. You (or Your) shall mean a person or an entity, and all other entities that control, are controlled by, or are under common control with the entity. Control for purposes of this definition means the direct or indirect power to cause the direction or management of such entity, whether by contract or otherwise, or (ii) ownership of fifty percent (50%) or more of the outstanding shares, or (iii) beneficial ownership of such entity.
+
+* Provided that You agree to the conditions described below, NCI grants You a non-exclusive, worldwide, perpetual, fully-paid-up, no-charge, irrevocable, transferable and royalty-free right and license in its rights in the Software to use, install, access, operate, execute, reproduce, copy, modify, translate, market, publicly display, publicly perform, and prepare derivative works of the Software, in any manner and for any purpose, and to have or permit others to do so; (ii) make, have made, use, practice, sell, and offer for sale, import, and/otherwise dispose of the Software (or portions thereof); (iii) distribute and have distributed to and by third parties the Software and any modifications and derivative works thereof; and (iv) sublicense the foregoing rights set out in , (ii) and (iii) to third parties, including the right to license such rights to further third parties. For sake of clarity, and not by way of limitation, NCI shall have no right of accounting or right of payment from You or Your sub-licensees for the rights granted under this License. This License is granted at no charge to You. Your downloading, copying, modifying, displaying, distributing or use of the Software constitutes acceptance of all of the terms and conditions of this License. If you do not agree to such terms and conditions, you have no right to download, copy modify, display, distribute or use the Software.
+
+* 1. Your redistributions of the source code for the Software must retain the above copyright notice, this list of conditions and the disclaimer and limitation of liability of Article 6 below. Your redistributions in object code form must reproduce the above copyright notice, this list of conditions and the disclaimer of Article 6 in the documentation and/or other materials provided with the distribution, if any.
+
+* 2. Your end-user documentation included with the redistribution, if any, must include the following acknowledgment: This product includes software developed
+by Stelligent. If You do not include such end-user documentation, You shall include this acknowledgment in the Software itself, wherever such third-party acknowledgments normally appear.
+
+* 3. You may not use the names "The National Cancer Institute", "NCI", NCICB, NCI CBIIT, "Stelligent", Automation for the People, LLC to endorse or promote products derived from this Software. This License does not authorize You to use any trademarks, service marks, trade names, logos or
+product names of either NCI, Stelligent except as required to comply with the terms of this License.
+
+* 4. For sake of clarity, and not by way of limitation, You may incorporate this Software into Your proprietary programs and into any third party proprietary
+programs. However, if You incorporate the Software into third party proprietary programs, You agree that You are solely responsible for obtaining any permission from such third parties required to incorporate the Software into such third party proprietary programs and for informing Your sub-licensees, including without limitation Your end-users, of their obligation to secure any required permissions from such third parties before incorporating the Software into such third party proprietary software programs. In the event that You fail to obtain such permissions, You agree to indemnify NCI for any claims against NCI by such third parties, except to the extent prohibited by law, resulting from Your failure to obtain such permissions.
+
+* 5. For sake of clarity, and not by way of limitation, You may add Your own copyright statement to Your modifications and to the derivative works, and You may provide additional or different license terms and conditions in Your sublicenses of modifications of the Software, or any derivative works of the Software as a whole, provided Your use, reproduction, and distribution of the Software otherwise complies with the conditions stated in this License.
+
+* 6. THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED WARRANTIES, (INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED. IN NO EVENT SHALL THE NATIONAL CANCER INSTITUTE, STELLIGENT OR THEIR AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 package gov.nih.nci.bda.provisioner;
+/**
+ *
+ * @author Mahidhar Narra
+ */
+
 import com.sshtools.j2ssh.ScpClient;
 import com.sshtools.j2ssh.SshClient;
 import com.sshtools.j2ssh.authentication.AuthenticationProtocolState;
@@ -68,7 +96,7 @@ public class EC2SystemInitiator
 	LOGGER.log(Level.INFO, "KEY FILE " + authenticationClient.getKeyfile());
 	if( ssh.authenticate(authenticationClient)== AuthenticationProtocolState.COMPLETE)
 	{
-	
+
 		LOGGER.log(Level.INFO, "Authetication Successful using key ");
 		SessionChannelClient  sc = ssh.openSessionChannel();
 		ScpClient scp = ssh.openScpClient();
@@ -130,14 +158,14 @@ public class EC2SystemInitiator
 		reboot.executeCommand("reboot");
 		reboot.getState().waitForState(ChannelState.CHANNEL_CLOSED);
 		reboot.close();
-		
+
 	}
 	else
 	{
 		LOGGER.log(Level.WARNING, "Authetication Failed for root ");
 	}
 	ssh.disconnect();
-	
+
 	Thread.sleep(100000);
 	SshClient ssh1 = new SshClient();
 	ssh1.connect(hostName, new IgnoreHostKeyVerification());
@@ -152,31 +180,31 @@ public class EC2SystemInitiator
 		ScpClient scp = ssh1.openScpClient();
 		scp.put(new File("build-hudson.xml").getAbsolutePath(), "", true);
 		scp.put(new File(".bash_profile").getAbsolutePath(), "", true);
-		
+
 		SessionChannelClient  bash1 = ssh1.openSessionChannel();
 		bash1.requestPseudoTerminal("ansi", 80, 24, 0, 0, "");
-		bash1.executeCommand(". .bash_profile >> profile.log");		
+		bash1.executeCommand(". .bash_profile >> profile.log");
 		bash1.getState().waitForState(ChannelState.CHANNEL_CLOSED);
 		bash1.close();
-				
+
 		SessionChannelClient  scb = ssh1.openSessionChannel();
-		scb.requestPseudoTerminal("ansi", 80, 24, 0, 0, "");		
+		scb.requestPseudoTerminal("ansi", 80, 24, 0, 0, "");
 		scb.executeCommand("ant -f build-hudson.xml >> build.log");
 		scb.getState().waitForState(ChannelState.CHANNEL_CLOSED);
-		scb.close(); 
-		
+		scb.close();
+
 	}
 	else
 	{
 		LOGGER.log(Level.WARNING, "Authetication Failed for hudsonuser");
 	}
-	
+
 	ssh1.disconnect();
 
 	SshClient ssh2 = new SshClient();
 
 	Thread.sleep(100000);
-	ssh2.connect(hostName, new IgnoreHostKeyVerification());	
+	ssh2.connect(hostName, new IgnoreHostKeyVerification());
 	if( ssh2.authenticate(authenticationClient)== AuthenticationProtocolState.COMPLETE)
 	{
 		SessionChannelClient  reboot = ssh2.openSessionChannel();
@@ -194,15 +222,15 @@ public class EC2SystemInitiator
 
 	//Authenticate
 	// Open up the private key file
-	
+
 	if( ssh4.authenticate(authenticationClient)== AuthenticationProtocolState.COMPLETE)
 	{
 		ScpClient scp = ssh4.openScpClient();
 		scp.put(new File("mysqld").getAbsolutePath(), "/etc/init.d/", true);
 		scp.put(new File("my.cnf").getAbsolutePath(), "/etc/", true);
-		
-		
-		
+
+
+
 		SessionChannelClient  startMysql = ssh4.openSessionChannel();
 		startMysql.requestPseudoTerminal("ansi", 80, 24, 0, 0, "");
 		startMysql.executeCommand("/etc/init.d/mysqld start");
@@ -213,11 +241,11 @@ public class EC2SystemInitiator
 		setRootMysql.requestPseudoTerminal("ansi", 80, 24, 0, 0, "");
 		setRootMysql.executeCommand("mysqladmin -u root password mysql");
 		setRootMysql.getState().waitForState(ChannelState.CHANNEL_CLOSED);
-		setRootMysql.close();		
-		
+		setRootMysql.close();
+
 	}
 	ssh4.disconnect();
-	
+
 	/*
 	SshClient ssh3 = new SshClient();
 
@@ -226,13 +254,13 @@ public class EC2SystemInitiator
 
 	//Authenticate
 	// Open up the private key file
-	
+
 	if( ssh3.authenticate(pwd)== AuthenticationProtocolState.COMPLETE)
 	{
 		ScpClient scp = ssh3.openScpClient();
 		scp.put(new File("start-hudson.sh").getAbsolutePath(), "", true);
-		
-		
+
+
 	   	SessionChannelClient session = ssh3.openSessionChannel();
 		session.requestPseudoTerminal("ansi", 80, 24, 0, 0, "");
 		session.executeCommand("sh start-hudson.sh >> start.log");
@@ -243,35 +271,35 @@ public class EC2SystemInitiator
 	ssh3.disconnect();
 */
   }
-/*  
+/*
   public void executeRemoteCommand(String command ) {
-	  try 
+	  try
 	  {
 			OutputStream out = session.getOutputStream();
 			out.write(command.getBytes());
-			out.flush(); 
+			out.flush();
 			InputStream in = session.getInputStream();
-			
+
 			List remoteOutput = IOUtils.readLines(in);
 			for (Iterator iter = remoteOutput.iterator(); iter.hasNext();)
-			{		
+			{
 				LOGGER.log(Level.INFO, (String) iter.next());
 			}
-*/			
-		/*	
+*/
+		/*
 		  byte buffer[] = new byte[255];
-   		  int read;   		  
-   		  while((read = in.read(buffer))>0 ) 
+   		  int read;
+   		  while((read = in.read(buffer))>0 )
    		  {
    		    response = new String(buffer, 0, read);
    			LOGGER.log(Level.INFO, (String) response);
    		  }
-   		   		     
+
 	}
-	catch(Exception e) 
-	{		
+	catch(Exception e)
+	{
 		e.printStackTrace();
     }
   }
-    */  
+    */
 }
