11f870a477822738f88670d54fe2edbfdf46d540
duvallp <duvallp@ncias-d1611-v.nci.nih.gov>
2009-07-22 22:04:16 +0000

    Updated email message with hostname
    Former-commit-id: 2cc32290c5b5016573e40177a8e4fd09c8373454

--- software/provisioner-app/grails-app/services/CloudClientService.groovy
+++ software/provisioner-app/grails-app/services/CloudClientService.groovy
@@ -42,10 +42,15 @@ class CloudClientService {
 					to msg.email
 					subject "Continuous Integration Server Ready!"
 					body """
-					Continuous Integration Server is Ready and configured. GO CRAZY!!!!.
-					The hostname is ${hostName}.
-					The username is 'hudsonuser' and password is 'password'.
-					Contract administrator if you are experiencing difficulties accessing the host.
+					Continuous Integration Server is ready and configured.
+					1) Open a new command prompt and type ssh hudsonuser@${hostName}
+					2) When prompted Are you sure you want to continue connecting?, type yes
+					3) When prompted, the password is password
+					4) From the command line, type ./hudson/application/apache-tomcat-5.5.20/bin/startup.sh
+					5) To view the build status, go to your web browser and type http://${hostName}:48080/hudson/?auto_refresh=true
+					6) After successful build/deployment (after ~10 minutes), start the cai2 container by going back to your command prompt (where you're connected to EC2) and type ./apps/cai2-dac/jboss-4.0.5.GA/bin/run.sh
+					7) After waiting for JBoss to start, open your web browser and type the following to launch the CAI2 application: http://${hostName}:46210/caintegrator2/
+					Contact administrator if you are experiencing difficulties accessing the host.
 					"""
 				}
 			}
