d75b831a3bdbe99cf0ecd46da427d70daacbdbd5
narram <narram@ncias-d1611-v.nci.nih.gov>
2009-11-24 22:22:51 +0000

    validate the certification data and send an email
    Former-commit-id: 28e8239f9a3a3efb1e923222959cf45edc2af42a

--- software/bda-dashboard/wiki-cli/src/main/java/gov/nih/nci/confluence/ErrorValidator.groovy
+++ software/bda-dashboard/wiki-cli/src/main/java/gov/nih/nci/confluence/ErrorValidator.groovy
@@ -18,16 +18,7 @@ class ErrorValidator {
 	
 	
 	public void validateData()
-	{	 
-	    String certificationTemplateFile = properties.getProperty("certification.template.file");//"Deployment_Status_Template"
-	    String certificationTemplateSpace = properties.getProperty("certification.template.space");//"test"
-	    String certificationPageFile = properties.getProperty("certification.page.file");//"page1"
-	    String certificationPageSpace = properties.getProperty("certification.page.space");//"confluence-cli-1.3.0.jar"
-	    String dashboardVersion = properties.getProperty("dashboard.release.version");//"1.0.0"
-	    String dashboardRevision = properties.getProperty("dashboard.revision.number");//"100"
-	    
-	    String dashboardRelease = "[" + dashboardVersion + "|#anchor|" + dashboardRevision + "]"
-
+	{
 	    String statement   = "select PRODUCT,CERTIFICATION_STATUS,SINGLE_COMMAND_BUILD,SINGLE_COMMAND_DEPLOYMENT,DATABASE_INTEGRATION,TEMPLATE_VALIDATION,PRIVATE_PROPERTIES,CI_BUILD,BDA_ENABLED,DEPLOYMENT_SHAKEOUT,COMMANDLINE_INSTALLER from PROJECT_CERTIFICATION_STATUS order by product desc"
 
 	    List projectRows = connection.rows(statement)	
@@ -49,7 +40,9 @@ class ErrorValidator {
 			String productUrl  = productString.substring(productString.indexOf("|")+1, productString.indexOf("]"));
 			String productName  = productString.substring(productString.indexOf("[")+1, productString.indexOf("|"));
  			println "productName:: " +productName
- 			StringBuffer mailString = new StringBuffer("The certification of the project ").append(productName).append(" failed for the below features ")
+ 			
+ 		
+ 			String mailString = properties.getProperty("mail.pre.template")
 
 			if(!certificationStatus.equals("(/)"))
 			{
@@ -59,47 +52,56 @@ class ErrorValidator {
 				{				
 					if (!singleCommandBuild.equals("(/)"))
 					{
-						mailString.append("\nSINGLE COMMAND BUILD FAILED");
+						mailString = mailString + properties.getProperty("mail.single.command.build")
+						mailString = mailString.replaceAll("ERROR_MESSAGE",singleCommandBuild.substring(singleCommandBuild.lastIndexOf("|")+1, singleCommandBuild.indexOf("]")))
 					}
 					if (!singleCommandDeployment.equals("(/)"))
 					{
-						mailString.append("\nSINGLE COMMAND DEPLOYMENT FAILED");
+						mailString = mailString + properties.getProperty("mail.single.command.deployment")
+						mailString = mailString.replaceAll("ERROR_MESSAGE",singleCommandDeployment.substring(singleCommandDeployment.lastIndexOf("|")+1, singleCommandDeployment.indexOf("]")))
 					}					
 					if (!databaseIntegration.equals("(/)"))
 					{
-						mailString.append("\nDATABASE INTEGRATION FAILED");
+						mailString = mailString + properties.getProperty("mail.database.integration")
+						mailString = mailString.replaceAll("ERROR_MESSAGE",databaseIntegration.substring(databaseIntegration.lastIndexOf("|")+1, databaseIntegration.indexOf("]")))
 					}
 					if (!templateValidation.equals("(/)"))
 					{
-						mailString.append("\nTEMPLATE VALIDATION FAILED");
+						mailString = mailString + properties.getProperty("mail.template.validation")
+						mailString = mailString.replaceAll("ERROR_MESSAGE",templateValidation.substring(templateValidation.lastIndexOf("|")+1, templateValidation.indexOf("]")))
 					}
 					if (!privateProperties.equals("(/)"))
 					{
-						mailString.append("\nPRAVATE PROPERTIES FAILED");
+						mailString = mailString + properties.getProperty("mail.private.properties")
+						mailString = mailString.replaceAll("ERROR_MESSAGE",privateProperties.substring(privateProperties.lastIndexOf("|")+1, privateProperties.indexOf("]")))
 					}
 					println "ciBuild :: " + ciBuild
 					if (ciBuild != null && !ciBuild.trim().equals("")) 
 					{
 						if (!ciBuild.substring(ciBuild.indexOf("[")+1, ciBuild.indexOf("|")).equals("(/)"))
 						{
-							mailString.append("\nCI BUILD FAILED");
+							mailString = mailString + properties.getProperty("mail.ci.build")
+							mailString = mailString.replaceAll("ERROR_MESSAGE",ciBuild.substring(ciBuild.lastIndexOf("|")+1, ciBuild.indexOf("]")))
 						}
 					}
 					else
 					{
-							mailString.append("\nCI BUILD FAILED");
+							mailString = mailString + properties.getProperty("mail.ci.build")
+							mailString = mailString.replaceAll("ERROR_MESSAGE","")
 					}
 					
 					if (!deploymentShakeout.equals("(/)"))
 					{
-						mailString.append("\nDEPLOYMENT SHAKEOUT FAILED");
+						mailString = mailString + properties.getProperty("mail.deployment.shakeout")
+						mailString = mailString.replaceAll("ERROR_MESSAGE",deploymentShakeout.substring(deploymentShakeout.lastIndexOf("|")+1, deploymentShakeout.indexOf("]")))
 					}
 					if (!commandLineInstaller.equals("(/)"))
 					{
-						mailString.append("\nCOMMANDLINE INSTALLER FAILED");
+						mailString = mailString + properties.getProperty("mail.commandLine.installer")
+						mailString = mailString.replaceAll("ERROR_MESSAGE",commandLineInstaller.substring(commandLineInstaller.lastIndexOf("|")+1, commandLineInstaller.indexOf("]")))
 					}
-					
-					sendEmailToProjectTeam(productName,mailString.toString())
+					mailString = mailString + properties.getProperty("mail.post.template")
+					sendEmailToProjectTeam(productName,mailString.replaceAll("INSERT_SVN_URL",productUrl))
 				}					
 			}
 			
@@ -133,19 +135,29 @@ class ErrorValidator {
 	public void sendEmailToProjectTeam(String projectName, String message )
 	{
 		// Get the POCs for the projectName
-		String devPocEmail,govPocEmail= null
-		
-		String pocStatement = "SELECT DEV_POC_EMAIL, GOV_POC_EMAIL FROM PROJECT_POC WHERE PROJECT_NAME='"+projectName + "'"
-		connection.eachRow(pocStatement) { row ->			  
-			devPocEmail    = row.DEV_POC_EMAIL;
-			govPocEmail = row.GOV_POC_EMAIL;	 
-		}	
-		MailSender ms = new MailSender()
-		ArrayList recipientList = new ArrayList()
-		println("Sending email to Dev Poc " +devPocEmail + " And GOV Poc " + govPocEmail)
-		recipientList.add(devPocEmail)
-		recipientList.add(govPocEmail)
-		ms.sendMessage(properties.getProperty("mail.hostname"),Integer.parseInt(properties.getProperty("mail.portnumber")),properties.getProperty("mail.send.address"),recipientList ,projectName.toUpperCase()  + " CERTIFICATION STATUS",message)
-	}	
-	
+		String devPocEmail,govPocEmail,devPoc= null
+		try
+		{		
+			message = message.replaceAll("INSERT_PROJECT_NAME",projectName)
+
+
+			String pocStatement = "SELECT DEV_POC, DEV_POC_EMAIL, GOV_POC_EMAIL FROM PROJECT_POC WHERE PROJECT_NAME='"+projectName + "'"
+			connection.eachRow(pocStatement) { row ->
+				devPoc    = row.DEV_POC;
+				devPocEmail    = row.DEV_POC_EMAIL;
+				govPocEmail = row.GOV_POC_EMAIL;	 
+			}
+			println ("Dev POC: " + devPoc)
+			message = message.replaceAll("DEV_POC_NAME",devPoc)
+			MailSender ms = new MailSender()
+			ArrayList recipientList = new ArrayList()
+			println("Sending email to Dev Poc " +devPocEmail + " And GOV Poc " + govPocEmail)
+			recipientList.add(devPocEmail)
+			recipientList.add(govPocEmail)
+			ms.sendMessage(properties.getProperty("mail.hostname"),Integer.parseInt(properties.getProperty("mail.portnumber")),properties.getProperty("mail.send.address"),recipientList , "BDA Certification status for " + projectName.toUpperCase(),message)
+		}
+		catch(Exception ex){
+			ex.printStackTrace()
+		}
+	}
 }
