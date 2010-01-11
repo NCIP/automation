8ff5103984f0f1c246b0915057f62a02519951f3
narram <narram@ncias-d1611-v.nci.nih.gov>
2010-01-11 19:00:38 +0000

    add override properties of cacoresdk project

--- certification-monitor/src/main/sql/insert_dashboard_properties.sql
+++ certification-monitor/src/main/sql/insert_dashboard_properties.sql
@@ -1031,6 +1031,7 @@ insert into dashboard_properties (dashboard_key, dashboard_value) values('cacore
 insert into dashboard_properties (dashboard_key, dashboard_value) values('cacoresdk.install.init.target','install:init');
 insert into dashboard_properties (dashboard_key, dashboard_value) values('cacoresdk.remote-upgrade.target','deploy:remote:upgrade');
 insert into dashboard_properties (dashboard_key, dashboard_value) values('cacoresdk.override.remote.properties','ssh.server.username=@remote.username@,ssh.server.hostname=@remote.hostname@,ssh.port=22,ssh.key.file=${user.home}/ssh-pub-key/id_dsa_bda,ssh.dir.temp=${user.home}/buildcertification/build/working/remote-deploy');
+insert into dashboard_properties (dashboard_key, dashboard_value) values('cacoresdk.override.properties','exclude.database=true,application.base.path.linux=${user.home}/apps/cacoresdk-app,jboss.server.hostname=localhost');
 
 insert into dashboard_properties (dashboard_key, dashboard_value) values('pro-ctcae.database.type','mysql');
 insert into dashboard_properties (dashboard_key, dashboard_value) values('pro-ctcae.svn.username','narram');
