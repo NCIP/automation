02100a10ca9209a00ddad1079f3f81eada6cc4b7
juliusp <juliusp@ncias-d1611-v.nci.nih.gov>
2010-01-27 16:10:04 +0000

    Removed a superfluous test that was used to get the build working.

--- bda-webapp/src/tests/unit/gov/nih/nci/bda/web/HelloControllerTest.java
+++ bda-webapp/src/tests/unit/gov/nih/nci/bda/web/HelloControllerTest.java
@@ -7,12 +7,6 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloControllerTest {
 
     @Test
-    public void testFoo() {
-        System.out.println("true = " + true);
-        assert (true);
-    }
-
-    @Test
     public void shouldAlwaysSendToHelloPage() throws Exception {
         HelloController controller = new HelloController();
         ModelAndView modelAndView = controller.handleRequest(null, null);
