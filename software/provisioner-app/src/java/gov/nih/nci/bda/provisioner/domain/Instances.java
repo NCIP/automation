156d935f234e71100a9e75432eae33c0609a1485
narram <narram@ncias-d1611-v.nci.nih.gov>
2010-02-07 23:55:37 +0000

    Former-commit-id: aeecd5f11f88de42a48788fa3a7f4d5b896ecb7f

---
+++ software/provisioner-app/src/java/gov/nih/nci/bda/provisioner/domain/Instances.java
@@ -0,0 +1,57 @@
+package gov.nih.nci.bda.provisioner.domain;
+
+import com.xerox.amazonws.ec2.InstanceType;
+
+public class Instances {
+	private int id;
+	private String instanceId;
+	private int  userId;
+	private String projectName;
+	private String instanceName;	
+	private String instanceType;
+	private String instanceStatus;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getInstanceId() {
+		return instanceId;
+	}
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getInstanceName() {
+		return instanceName;
+	}
+	public void setInstanceName(String instanceName) {
+		this.instanceName = instanceName;
+	}
+	public String getInstanceType() {
+		return instanceType;
+	}
+	public void setInstanceType(String instanceType2) {
+		this.instanceType = instanceType2;
+	}
+	public String getInstanceStatus() {
+		return instanceStatus;
+	}
+	public void setInstanceStatus(String instanceStatus) {
+		this.instanceStatus = instanceStatus;
+	}	
+}
