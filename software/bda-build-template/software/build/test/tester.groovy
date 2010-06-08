ec25430c26de9974417404453fec22742f76a4f7
saksass <saksass@ncias-d1611-v.nci.nih.gov>
2010-06-08 17:24:49 +0000

    gonna test in windows
    Former-commit-id: d209dcc55b024b45c620aa732bb85ed4a184d4bc

---
+++ software/bda-build-template/software/build/test/tester.groovy
@@ -0,0 +1,63 @@
+#!/usr/bin/env groovy
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+import java.util.Properties
+import jva.io.*
+
+/*
+def sout = new StringBuffer()
+def serr = new StringBuffer()
+
+antTest1 = 'ant deploy:local:install'.execute()
+antTest1.consumeProcessOutput(sout, serr)
+antTest1.waitFor()
+println "=====Standard Out======"
+sout.toString().eachLine{println "\t" + it}
+println "=====Standard Out======"
+serr.toString().eachLine{println "\t" + it}
+*/
+
+def antFail = new AntBuilder()
+try{
+antFail.exec(
+	outputProperty: "sout",
+	errorProperty: "serr",
+	resultProperty: "rc",
+	failonerror: "true",
+	dir: "/data/home/ssaksa/src/trunk/bda-build-template/software/build",
+	executable: "ant"
+	)
+	{
+		arg(line: "junktarget")
+		env(key: "TESTENVVAR", value: "TESTENVVALUE")
+	}
+}
+catch (e) {}
+println "\n==== Standard Out ====" 
+println antFail.project.properties.sout
+println "\n==== Standard Err ====" 
+println antFail.project.properties.serr
+println "\n=== Return Code ====" 
+println antFail.project.properties.rc
+
+def antPass = new AntBuilder()
+antPass.exec(
+	outputProperty: "sout",
+	errorProperty: "serr",
+	resultProperty: "rc",
+	failonerror: "true",
+	dir: "/data/home/ssaksa/src/trunk/bda-build-template/software/build",
+	executable: "ant"
+	)
+	{
+		arg(line: "deploy:local:install")
+		env(key: "TESTENVVAR", value: "TESTENVVALUE")
+	}
+println "\n==== Standard Out ====" 
+println antPass.project.properties.sout
+println "\n==== Standard Err ====" 
+println antPass.project.properties.serr
+println "\n=== Return Code ====" 
+println antPass.project.properties.rc
+
