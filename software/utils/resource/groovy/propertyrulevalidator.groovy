3334952302bc0073a7a5d86f6ad70c52e64e6ad6
saksass <saksass@ncias-d1611-v.nci.nih.gov>
2010-06-16 10:24:18 +0000

    getting closer
    Former-commit-id: b2886728ca4e3ec364e77fa2947d8fd2a9646bd9

--- software/utils/resource/groovy/propertyrulevalidator.groovy
+++ software/utils/resource/groovy/propertyrulevalidator.groovy
@@ -1,97 +1,47 @@
 #!/usr/bin/env groovy
 import java.util.regex.Matcher
 import java.util.regex.Pattern
+import org.apache.log4j.*
 
 class propertyRuleValidator
 {
 	def antProps
-	def antPropsConv
 	def failureMessages=""
 	def debug=false
 	def rulesFileLocation
+	def log4jPattern="%d{ABSOLUTE} %-5p [%c{1}] %m%n"
+
+	Logger log
+
 	public propertyRuleValidator(antPropsIn, File rulesFileLocationIn)
 	{
+		/*
+		def pattern = new PatternLayout(log4jPattern)
+		BasicConfigurator.configure(new ConsoleAppender(pattern))
+		LogManager.rootLogger.level=Level.INFO
+		log = Logger.getInstance(propertyRuleValidator.class)
+
+		log.info "Test log4j"
+		*/
+
 		antProps=antPropsIn
 		rulesFileLocation=rulesFileLocationIn
-		println antProps.getClass()
 		//System.getProperties().sort{it.key}.each{println it.key +"\t"+ it.value}
 		if (antProps['groovy.debug'])
 		{
 			debug=true;
 			println "Debug Enabled..."
 		}
-		/*
-		antProps.each
-		{ k, v -> 
-			println k.getClass()
-			println v.getClass()
-			def tmpKey = k.replaceAll('\\.','')
-			println "debug"
-			println tmpKey.getClass()
-			antPropsConv[tmpKey]=v
-			println "debug"
-		}
-		antPropsConv.sort{it.key}.each{println it}
-		*/
 	}
 			
-	public void testAntEvaluate ()
+	private antEvaluateAntProperties()
 	{
-		//antProps.sort{it.key}.each{println it}
-		//System.getProperties().each{println it.key +"\t"+ it.value}
-		println antProps["jboss.server.name"]
-		Binding binding = new Binding();
-		GroovyShell shell = new GroovyShell(binding);
-		def antPattern1='jbossServerName ==~ /default/'
-		if (antProps["jboss.server.name"] ==~ /[\w\d\-\_]+/) println "pass patterns should work"
-		binding.setVariable("jbossServerName", new String(antProps["jboss.server.name"]));
-		if (shell.evaluate(antPattern1)) println  "antPattern1 passed"
-	}
-
-	private static void testShellEvaluate ()
-	{
-		Binding binding = new Binding();
-		GroovyShell shell = new GroovyShell(binding);
-		binding.setVariable("vabc", new String("abc"));
-		binding.setVariable("vdef", new String("def"));
-		def passPattern1="vabc ==~ /.*a.*/ && vdef ==~ /.*d.*/"
-		def passPattern2="vabc ==~ /.*b.*/ && vdef ==~ /.*e.*/"
-		def failPattern1="vabc ==~ /.*d.*/ && vdef ==~ /.*g.*/"
-		def failPattern2="vabc ==~ /.*d.*/ && vdef ==~ /.*d.*/"
-		
-		if ("abc" ==~ /.*a.*/) println "pass patterns should work"
-		if (shell.evaluate(passPattern1)) println  "passPattern1 passed"
-		if (shell.evaluate(passPattern2)) println  "passPattern2 passed"
-		if (!shell.evaluate(failPattern1)) println  "failPattern1 failed"
-		if (!shell.evaluate(failPattern2)) println  "failPattern2 failed"
-	}
-	private void antEvaluateAntProperties()
-	{
+		//log.info "Entering antEvaluateAntProperties"
 		def xml = new XmlParser().parse(rulesFileLocation)
-
-		/*
-		println xml.getClass()
-		println xml.methods.each{println it}
-		println xml.name()
-		println xml.attributes()
-		println xml.children()
-		println "xml.each => "
-		def i=0
-		xml.each
-		{ prop->
-			println "property[${i}] -> " + prop.@name
-			i++
-		}
-
-		println xml.property[0].name()
-		println xml.property[0].attributes()
-		println xml.property[0].children()
-		*/
 		def o=0
 		xml.property.each
 		{ prop->
-			println "property[${o}] -> " + prop.@name
-			//println prop.rules.children()
+			if(debug) println "property[${o}] -> " + prop.@name
 			def propertyName=prop.@name
 			def i=0
 			prop.rules.rule.each
@@ -100,60 +50,55 @@ class propertyRuleValidator
 				def ruleDesc=r.description.text()
 				def ruleFailMsg=r.'fail-message'.text()
 
-				println "\trule[${i}] -> " + r.name.text()
+				if(debug) println "\trule[${i}] -> " + r.name.text()
 				i++
-				if(debug) println "class-> " + r.conditions.getClass()
+				// println "class-> " + r.conditions.getClass()
 				def ruleCondition=antRecurseConditions(r.conditions.'*', "")
-				println "\t\tCondition -> " + ruleCondition
-				println prop.@name + "\t" + antProps[prop.@name] + "\t" + antProps[prop.@name].getClass()
+				if(debug) println "\t\tCondition -> " + ruleCondition
+				if(debug) println prop.@name + "\t" + antProps[prop.@name] + "\t" + antProps[prop.@name].getClass()
 				def result=antEvaluateRule(propertyName, ruleCondition )
 				if (result)
 				{
-					println "Rule ${ruleName} for property ${propertyName} passed."
+					if(debug) println "Rule ${ruleName} for property ${propertyName} passed."
 				} else
 				{
-					println "Rule ${ruleName} for property ${propertyName} FAILED."
-					println "\t" + ruleFailMsg
-					failureMessages = failureMessages + ruleFailMsg
+					if(debug) println "Property - ${propertyName}\tRule - ${ruleName} FAILED."
+					if(debug) println "\t" + ruleFailMsg
+					failureMessages = failureMessages + "== Property [${propertyName}]  Rule [${ruleName}] failed.\n" + ruleFailMsg
 				}
 			}
 			o++
 		}
+		return failureMessages
 	}
 	private  antRecurseConditions(groovy.util.NodeList curNode, String cond)
 	{
 		def tempCondString=""
-		if(debug) println "in antRecurseConditions"
+		// println "in antRecurseConditions"
 		def childCount=curNode.size()
-		if(debug) println childCount
+		// println childCount
 		def cnt=0
 		curNode.each
 		{ c ->
 			cnt++
-			if(debug) println "NodeName -> '" + c.name() + "'"
+			// println "NodeName -> '" + c.name() + "'"
 			def condType = c.name()
 			if (condType == "or")
 			{
-				if(debug) println "in or"
+				// println "in or"
 				tempCondString = tempCondString + "(" + antRecurseConditions(c.'*', " || ") + ")"
-				if(debug) println "retrun or"
+				// println "retrun or"
 						
 			} else if (condType == "and" )
 			{
-				if(debug) println "in and"
+				// println "in and"
 				tempCondString = tempCondString + "(" + antRecurseConditions(c.'*', " && ") + ")"
-				if(debug) println "retrun and"
+				// println "retrun and"
 			} else if (condType == "condition")
 			{
-				if(debug) println "in condition"
-				/*
-				println c.getClass()
-				println c.name()
-				println c.parent().name()
-				println c.children()
-				*/
-				if(debug) println "\t\tcondition - > " + c.text()
-				if(debug) println "${childCount}\t${cnt}"
+				// println "in condition"
+				// println "\t\tcondition - > " + c.text()
+				// println "${childCount}\t${cnt}"
 
 				def condConv=antStripDot(c.text())
 
@@ -164,16 +109,16 @@ class propertyRuleValidator
 				{
 					tempCondString=tempCondString + condConv 
 				}
-				if(debug) println "\t\ttempCondString -> " + tempCondString
-				if(debug) println "retrun condition"
+				// println "\t\ttempCondString -> " + tempCondString
+				// println "retrun condition"
 			
 			} else
 			{
-				if(debug) println "Invalid condition syntax."
+				// println "Invalid condition syntax."
 			}
 		}
 		return tempCondString 
-		if(debug) println "outside of each"
+		// println "outside of each"
 	}
 	private  antEvaluateRule(String propertyName, String ruleCondition)
 	{
@@ -201,4 +146,9 @@ def rulesFileLocation=new File(args[0]).getAbsoluteFile()
 def prv = new propertyRuleValidator(properties, rulesFileLocation)
 //prv.testAntEvaluate()
 //prv.antTestReadXML()
-prv.antEvaluateAntProperties()
+def failMsgs=prv.antEvaluateAntProperties()
+//println "====== Property Validation Errors ======"
+//println failMsgs
+properties["property.rule.validator.failed"]="true"
+properties["property.rule.validator.msg"]=failMsgs
+
