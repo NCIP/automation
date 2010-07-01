ea8a30ca0dba7e1b1a6abfdc3cc07ce9c5443bbd
tbassett <tbassett@ncias-d1611-v.nci.nih.gov>
2010-07-01 14:04:00 +0000

    Added testing framework

---
+++ wiki-cli/src/main/java/test/gov/nih/nci/confluence/BuildStatusUpdaterTest.groovy
@@ -0,0 +1,21 @@
+package test.gov.nih.nci.confluence
+
+import groovy.util.GroovyTestCase
+import org.junit.Test
+import gov.nih.nci.confluence.BuildStatusUpdater
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hudsonuser
+ * Date: Jun 30, 2010
+ * Time: 5:41:02 PM
+ * To change this template use File | Settings | File Templates.
+ * this is a test
+ */
+class BuildStatusUpdaterTest extends GroovyTestCase {
+
+  void testCtor() {
+    BuildStatusUpdater target = new BuildStatusUpdater();
+    assertNotNull (target);
+  }
+}
