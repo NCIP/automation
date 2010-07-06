5384aabb3fd99ea0990908df355b7b239e485a96
tbassett <tbassett@ncias-d1611-v.nci.nih.gov>
2010-07-06 16:14:58 +0000

    remove redundant code

--- wiki-cli/src/main/java/test/gov/nih/nci/confluence/BuildStatusUpdaterTest.groovy
+++ wiki-cli/src/main/java/test/gov/nih/nci/confluence/BuildStatusUpdaterTest.groovy
@@ -40,31 +40,6 @@ class BuildStatusUpdaterTest extends GroovyTestCase {
     assertNotNull (target);
   }
 
-  void testGetWikiMarkupForRow() {
-    BuildStatusUpdater target = new BuildStatusUpdater();
-
-    String actual = target.getWikiMarkupForRow(PRODUCT, BDA_ENABLED, CERTIFICATION_STATUS, SINGLE_COMMAND_BUILD, SINGLE_COMMAND_DEPLOYMENT, DATABASE_INTEGRATION, REMOTE_UPGRADE, TEMPLATE_VALIDATION, PRIVATE_PROPERTIES, CI_BUILD, DEPLOYMENT_SHAKEOUT, COMMANDLINE_INSTALLER) ;
-
-//    make sure it starts with a row marker
-    assertStartsWith("|| ", actual);
-
-//    make sure it contains the Product
-    assertContains(PRODUCT + " | ", actual);
-
-//    make sure it contains the Certification status
-    assertContains(CERTIFICATION_STATUS + " | ", actual);
-
-//    make sure it contains the BDA Enabled status
-    assertContains(BDA_ENABLED + " | ", actual);
-
-//    make sure it contains Single Command Build
-    assertContains(SINGLE_COMMAND_BUILD + " | ", actual);
-
-//    make sure it ends with a row marker
-    assertEndsWith(" ||", actual);
-
-  }
-
   void testGetWikiMarkupForRowStartsWithRowMarker() {
     BuildStatusUpdater target = new BuildStatusUpdater();
 
