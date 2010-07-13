a41bf7e7ebb851722b0f21618eb14188aca17072
tbassett <tbassett@ncias-d1611-v.nci.nih.gov>
2010-07-13 14:40:56 +0000

    add not null to cert date

--- certification-monitor/src/main/sql/create_table_project_poc.sql
+++
@@ -1,9 +0,0 @@
-drop table IF EXISTS project_poc;
-create table project_poc (
-	PROJECT_NAME varchar (100) ,
-	DEV_POC varchar (200),
-	DEV_POC_EMAIL varchar (200),
-	GOV_POC varchar (200),
-	GOV_POC_EMAIL varchar (200),
-	BDA_POC_EMAIL varchar (200)
-);
