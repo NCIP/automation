eaa6008d09b3ed21e250e37bec527581092c5ed8
tbassett <tbassett@ncias-d1611-v.nci.nih.gov>
2010-07-20 23:02:58 +0000

    add test case (empty)

---
+++ certification-monitor/src/main/java/test/gov/nih/nci/bda/certification/domain/TargetLookupTest.java
@@ -0,0 +1,125 @@
+package test.gov.nih.nci.bda.certification.domain;
+
+import gov.nih.nci.bda.certification.domain.TargetLookup;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
+
+/**
+ * TargetLookup Tester.
+ *
+ * @author <Authors name>
+ * @since <pre>07/20/2010</pre>
+ * @version 1.0
+ */
+public class TargetLookupTest extends TestCase {
+    public TargetLookupTest(String name) {
+        super(name);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     *
+     * Method: getId()
+     *
+     */
+    public void testGetId() throws Exception {
+        //TODO: Test goes here...
+        TargetLookup target = new TargetLookup();
+        
+    }
+
+    /**
+     *
+     * Method: setId(int id)
+     *
+     */
+    public void testSetId() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     *
+     * Method: getTargetName()
+     *
+     */
+    public void testGetTargetName() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     *
+     * Method: setTargetName(String targetName)
+     *
+     */
+    public void testSetTargetName() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     *
+     * Method: getMapName()
+     *
+     */
+    public void testGetMapName() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     *
+     * Method: setMapName(String mapName)
+     *
+     */
+    public void testSetMapName() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     *
+     * Method: getIsValue()
+     *
+     */
+    public void testGetIsValue() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     *
+     * Method: setIsValue(String isValue)
+     *
+     */
+    public void testSetIsValue() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     *
+     * Method: getIsOptional()
+     *
+     */
+    public void testGetIsOptional() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     *
+     * Method: setIsOptional(String isOptional)
+     *
+     */
+    public void testSetIsOptional() throws Exception {
+        //TODO: Test goes here...
+    }
+
+
+
+    public static Test suite() {
+        return new TestSuite(TargetLookupTest.class);
+    }
+}
