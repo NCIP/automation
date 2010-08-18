71c9432290f81a49e5156c913fe9ffb2ddfb83e7
tbassett <tbassett@ncias-d1611-v.nci.nih.gov>
2010-08-18 19:17:06 +0000

    https://tracker.nci.nih.gov/browse/BDA-540
    throw exception to fail the build

--- wiki-cli/src/main/java/gov/nih/nci/confluence/BuildStatusUpdater.groovy
+++ wiki-cli/src/main/java/gov/nih/nci/confluence/BuildStatusUpdater.groovy
@@ -481,20 +481,10 @@ class BuildStatusUpdater {
       cmdStr = ["sh", "-c", "${cmd}"]
 
     Process process = cmdStr.execute()
-    process.waitForOrKill(1000 * 240) ;
 
-
-    println "${process.text} ${process.err.text}"
-
-    println "ERROR VALUE: ${process.exitValue()}"
-    println "ERROR TEXT:  ${process.err.text}"
-
-    // wait for 4 minutes
-
-    if (process.exitValue() != 0) {
+    if (process.waitFor() != 0) {
       throw new Exception("Command failure code: ${process.exitValue()}\n ${process.err.text()}\n ${cmd}")
     }
-
 
     return process
 
