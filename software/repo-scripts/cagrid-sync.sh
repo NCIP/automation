393ad1ec472959f9dcc07207fcc0022fb4e621c4
saksass <saksass@ncias-d1611-v.nci.nih.gov>
2010-09-20 14:31:00 +0000

    udpate days
    Former-commit-id: 58985090846e37bc8326c15ad57f07c71e23b299

--- software/repo-scripts/cagrid-sync.sh
+++ software/repo-scripts/cagrid-sync.sh
@@ -2,13 +2,13 @@ if [ $# -ne 2 ]; then
 echo "Usage : cagrid-sync.sh <svn.user> <svn.password>"
 exit 1
 fi
-#wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-1.2-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tmp/wget http://software.cagrid.org/repository-1.2/
-#wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-1.2.0.1-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tmp/wget http://software.cagrid.org/repository-1.2.0.1/
-#wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-1.3.0.1-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tmp/wget http://software.cagrid.org/repository-1.3.0.1/
-#wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-1.3.0.2-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tmp/wget http://software.cagrid.org/repository-1.3.0.2/
-#wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-1.3-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tmp/wget http://software.cagrid.org/repository-1.3/
-#wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-1.4-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tmp/wget http://software.cagrid.org/repository-1.4/
-#wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tp/wget http://software.cagrid.org/repository/
+wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-1.2-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tmp/wget http://software.cagrid.org/repository-1.2/
+wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-1.2.0.1-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tmp/wget http://software.cagrid.org/repository-1.2.0.1/
+wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-1.3.0.1-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tmp/wget http://software.cagrid.org/repository-1.3.0.1/
+wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-1.3.0.2-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tmp/wget http://software.cagrid.org/repository-1.3.0.2/
+wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-1.3-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tmp/wget http://software.cagrid.org/repository-1.3/
+wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-1.4-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tmp/wget http://software.cagrid.org/repository-1.4/
+wget -r  -l 100 --no-parent --reject "index.html*" -o /maven/logs/cagrid-`date "+%Y%m%d-%H%d%S"`.log -P /maven/tp/wget http://software.cagrid.org/repository/
 
 cd /maven/tmp/wget/software.cagrid.org
 cp -r * /maven/cagrid-repo-copy
