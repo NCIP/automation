fac5696879777c110b6ea2531e3944f2c29e5a8e
saksass <saksass@ncias-d1611-v.nci.nih.gov>
2010-09-21 20:56:31 +0000

    added file list update stuff
    Former-commit-id: 850427fd3078f14ebd9d13c65cf4ea7cb03af76e

--- software/repo-scripts/ivy-repo-flist.sh
+++ software/repo-scripts/ivy-repo-flist.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 cd /maven/legacy-ivy-repo/ivy-repo
+echo updating `pwd`
+svn update
 find . -name "*.jar" |sort  > file_list.txt
 svn --username saksass commit -m "Updated automatically" file_list.txt
 
 cd /maven/legacy-ivy-repo
+echo updating `pwd`
+svn update
 find techstack* -type f |grep -v "\.svn" |sort > file_list.txt
 svn --username saksass commit -m "Updated automatically" file_list.txt
 
 cd /maven/cbiit-ivy-repo
+echo updating `pwd`
+svn update
 find . -name "*.jar" |sort  > file-list.txt
 svn --username bda_user commit -m "Updated automatically" file-list.txt
 
